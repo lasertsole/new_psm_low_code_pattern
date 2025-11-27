2cf07149f7cb890084931bd461342aea6edac444
MOYE <56973455+lasertsole@users.noreply.github.com>
2025-11-27 10:08:42 +0800

    Update index.ts

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,14 @@
 export interface ComponentProps{
     [key: string]: string | number | boolean | null | undefined | ComponentProps;
-    
+}
+
+export enum UPDATE_COMPONENT_ENUM {
+  /** 新增 */
+  ADD = 1,
+  /** 删除 */
+  DEL = 2,
+  /** 修改 */
+  MOD = 4,
+  /** 选中 */
+  SEL = 8
 }
