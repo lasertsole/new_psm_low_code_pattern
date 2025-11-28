3801abc8c0d7f74697265ffdedc11817b1066b1d
MOYE <56973455+lasertsole@users.noreply.github.com>
2025-11-28 08:56:32 +0800

    Update doc.ts

--- app/composables/doc.ts
+++ app/composables/doc.ts
@@ -1,3 +1,9 @@
 import * as Y from 'yjs';
+import { WebsocketProvider } from 'yjs/provider/websocket.js'
 
 const ydoc = new Y.Doc();
+const wsProvider = new WebsocketProvider('ws://localhost:8000', 'my-roomname', ydoc);
+
+wsProvider.on('status', event => {
+  console.log(event.status) // logs "connected" or "disconnected"
+})
